90a68438cd917b8afd5de03240b2f8dbbe7cdf76
channg <xssql@foxmail.com>
2018-07-23 23:28:38 +0800

    dev:chore

--- src/log.js
+++ src/log.js
@@ -1,5 +1,8 @@
 require( "colors")
-
+/**
+ * show log by keys
+ * @type {{NOKEY: module.exports.NOKEY}}
+ */
 module.exports = {
   NOKEY:function () {
     console.log('error: No package name was entered.'.red)
---
+++ src/process.js
@@ -0,0 +1,51 @@
+const inquirer = require('inquirer')
+module.exports = q
+
+var questions = [
+  {
+    type: 'input',
+    name: 'first_name',
+    message: "What's your first name"
+  },
+  {
+    type: 'input',
+    name: 'last_name',
+    message: "What's your last name",
+    default: function() {
+      return 'Doe';
+    }
+  },
+  {
+    type: 'input',
+    name: 'fav_color',
+    message: "What's your favorite color",
+    transformer: function(color, answers, flags) {
+      const text = color;
+      if (flags.isFinal) {
+        return text + '!';
+      }
+      return text;
+    }
+  },
+  {
+    type: 'input',
+    name: 'phone',
+    message: "What's your phone number",
+    validate: function(value) {
+      var pass = value.match(
+        /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+      );
+      if (pass) {
+        return true;
+      }
+
+      return 'Please enter a valid phone number';
+    }
+  }
+];
+function q() {
+  inquirer.prompt(questions).then(answers => {
+    console.log(JSON.stringify(answers, null, '  '));
+  });
+}
+q()
