7989be031fa29ba5da17aa0b4c076c7c83f79f08
unknown <xssql@foxmail.com>
2018-07-24 21:18:01 +0800

    chore

--- src/process.js
+++ src/process.js
@@ -1,51 +1,156 @@
+#!/usr/bin/env node --inspect-brk
 const inquirer = require('inquirer')
+const config = require('./commadConfig.js')
 module.exports = q
+/**
+ *  jump is a string of the inquire key
+ * @type {null}
+ */
+let jump = null
+/**
+ * questionIndex  is a number of current inquire
+ * @type {null}
+ */
+let questionIndex = null
+/**
+ * current key ,type is String
+ * @type {null}
+ */
+let keyG = null
 
-var questions = [
-  {
-    type: 'input',
-    name: 'first_name',
-    message: "What's your first name"
-  },
-  {
-    type: 'input',
-    name: 'last_name',
-    message: "What's your last name",
-    default: function() {
-      return 'Doe';
-    }
-  },
-  {
-    type: 'input',
-    name: 'fav_color',
-    message: "What's your favorite color",
-    transformer: function(color, answers, flags) {
-      const text = color;
-      if (flags.isFinal) {
-        return text + '!';
-      }
-      return text;
-    }
-  },
-  {
-    type: 'input',
-    name: 'phone',
-    message: "What's your phone number",
-    validate: function(value) {
-      var pass = value.match(
-        /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
-      );
-      if (pass) {
-        return true;
-      }
+let coirjson = null
+let qKeyArray = null
 
-      return 'Please enter a valid phone number';
-    }
-  }
-];
-function q() {
-  inquirer.prompt(questions).then(answers => {
-    console.log(JSON.stringify(answers, null, '  '));
-  });
+function q(j) {
+  coirjson = j
+  qKeyArray = initQuestions(j)
+  shwoInquire()
+  /*  console.log(question)
+    inquirer
+      .prompt(
+        question
+      )
+      .then(answers => {
+        console.log(answers)
+      });*/
 }
-q()
+
+
+/**
+ * get Array of key ,and sort
+ * @param coirjson
+ * @returns {Array}
+ */
+function initQuestions(coirjson) {
+  let qKeyArray = []
+  /**
+   * coir2.x coir.json
+   */
+  if (coirjson.inquire) {
+    for (let item in coirjson.inquire) {
+      qKeyArray.push(item)
+    }
+    qKeyArray.sort()
+    return qKeyArray
+  }
+}
+
+/**
+ * get questions
+ * @param qKeyArray
+ * @param coirjson
+ * @param index
+ * @returns {Array}
+ */
+function getNextQuestions(qKeyArray, coirjson, index, jump) {
+  let questions = []
+  
+  if (index===null) {
+    index = 0
+  } else {
+    index++
+  }
+  
+  questionIndex = index
+  if (jump) {
+    index = qKeyArray.indexOf(jump)
+  }
+  let key = qKeyArray[index]
+  /**
+   * next inquire not alive
+   */
+  if(!key){
+    return questions
+  }
+  keyG = key
+  let item = coirjson[key]
+  let type = item.type || 'input'
+  if (type === 'input') {
+    questions.push(typeInput(item, key))
+  }
+  return questions
+}
+
+/**
+ * type = input
+ * @param item
+ */
+function typeInput(item, key) {
+  let question = {}
+  question.name = config.prefix + key
+  question.type = item.type
+  question.message = item.question
+  question.default = item.default
+  question.validate = function (value) {
+    let check = 'try again'
+    if (item.output.length) {
+      item.output.forEach((i) => {
+        if (check!==true) {
+          check = new RegExp(i.test).test(value)
+          if(check===false){
+            check = 'Failed validation, please re-enter.'
+          }
+        }
+      })
+    } else {
+      check = new RegExp(item.output.test).test(value)
+      if(check===false){
+        check = 'Failed validation, please re-enter.'
+      }
+    }
+    return check
+  }
+  return question
+}
+
+
+function shwoInquire() {
+  let qs = getNextQuestions(qKeyArray, coirjson.inquire, questionIndex, jump)
+  if (qs.length > 0) {
+    point(qs).then((answer)=>{
+      shwoInquire()
+    })
+  }
+}
+
+/**
+ * Gets user input at the end
+ * @param qs
+ * @returns {Promise}
+ */
+function point(qs) {
+  return new Promise((resolve) => {
+    inquirer
+      .prompt(
+        qs
+      )
+      .then(answers => {
+        console.log(answers)
+        resolve(answers)
+      });
+  })
+}
+
+
+q(require('./inquire.json'))
+
